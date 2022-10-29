b3dede9f2ff9e470f318a44ca277e57e4451b009
HarveyBurton96 <harvey.burton@sky.com>
2022-10-29 10:56:39 +0000

    Initial commit

--- reduce.js
+++ reduce.js
@@ -6,7 +6,19 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+    "Accumulator: ", acc,
+    "Current Value: ", curr,
+    "Total: ", acc+curr
+  )
+  return acc + curr
+  }, 0);
+console.log(sum);
 
+// simple version 
+// let sum = nums.reduce((acc, curr) => acc + curr, 0);
 
 const teamMembers = [
   {
@@ -32,6 +44,18 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {})
+
+console.log(experienceByProfession);
